7fe7f3bcb90e3982b6d9c9203a7ba02abc4a516e
Nathaniel Rudenberg <nathaniel.rudenberg@gordon.edu>
2020-10-03 00:02:01 -0400

    Add license

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -10,6 +10,8 @@ export default class Home extends Component {
         For each pair, the symbol on the right represents the voiced counterpart.
         <ConsonantChart />
         Shaded areas represent articulations thought to be impossible to produce.
+        <br/>
+        The sounds on this page are from the <a href="https://en.wikipedia.org/wiki/IPA_pulmonic_consonant_chart_with_audio">Wikipedia IPA pulmonic chart</a> and are licensed under <a href="https://creativecommons.org/licenses/by-sa/3.0/deed.en">CC BY-SA 3.0</a>.
       </>
     );
   }
