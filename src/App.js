4b7f409dc88fd374f7c65011f17bffc31f37ba33
Nathaniel Rudenberg <nathaniel.rudenberg@gordon.edu>
2020-10-04 22:29:05 -0400

    Add app bar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Router, Route, Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import './App.css';
 import routes from './routes';
+import AppBar from './components/AppBar';
 
 class App extends React.Component {
   constructor(props) {
@@ -13,26 +14,29 @@ class App extends React.Component {
 
   render() {
     return (
-      <Router history={this.history}>
-        <section className='app-wrapper'>
-          <main className='app-main'>
-            <Switch>
-              {routes.map(route => (
-                <Route
-                  key={route.path}
-                  path={route.path}
-                  exact={route.exact}
-                  render={props => (
-                    <div className='app-main-container'>
-                      <route.component {...props} />
-                    </div>
-                  )}
+      <>
+        <Router history={this.history}>
+          <section className='app-wrapper'>
+            <AppBar />
+            <main className='app-main'>
+              <Switch>
+                {routes.map(route => (
+                  <Route
+                    key={route.path}
+                    path={route.path}
+                    exact={route.exact}
+                    render={props => (
+                      <div className='app-main-container'>
+                        <route.component {...props} />
+                      </div>
+                    )}
                   />
-              ))}
-            </Switch>
-          </main>
-        </section>
-      </Router>
+                ))}
+              </Switch>
+            </main>
+          </section>
+        </Router>
+      </>
     );
   }
 }
---
+++ src/components/AppBar/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './appbar.scss';
+
+export default class AppBar extends Component {
+    render() {
+        return (
+            <div className='header'></div>
+        )
+    }
+}
