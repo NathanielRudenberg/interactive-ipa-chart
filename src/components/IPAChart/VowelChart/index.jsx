dda7fd7f0148c4a2b83e412b681775e0a7a1a33b
Nathaniel Rudenberg <nathaniel.rudenberg@gordon.edu>
2025-10-01 13:51:20 -0400

    Add a failsafe in case the API isn't reachable

--- src/components/IPAChart/VowelChart/index.jsx
+++ src/components/IPAChart/VowelChart/index.jsx
@@ -64,7 +64,7 @@ export default function VowelChart() {
                 }
                 if (phonemes.length > 0) {
                     let vowelElement;
-                    if (languageName === 'default') {
+                    if (languageName === 'default' || formantValues[vowelName] === undefined) {
                         vowelElement = (
                             <Vowel language={languageName} key={vowelName} name={vowelName} className={vowelName + " vowel"}>
                                 {phonemes.length > 0 ? phonemes[0].symbol : vowelSymbols[vowelName]}
