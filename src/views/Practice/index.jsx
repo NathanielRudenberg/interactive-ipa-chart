985b82d6231f92fe44acac4b4e53a62e88c1e9ad
Nathaniel Rudenberg <nathaniel.rudenberg@gordon.edu>
2025-10-05 14:50:19 -0400

    Keep text from filling the whole screen on desktop

--- src/views/Practice/index.jsx
+++ src/views/Practice/index.jsx
@@ -5,24 +5,26 @@ import VowelCalibrator from '../../components/VowelCalibrator';
 
 export default function Practice() {
     return (
-        <div className="page-text">
+        <>
             <p></p>
             <div id="plot-container" />
             {/* <Python /> */}
-            <h1>Practice Your Pronunciation</h1>
-            <p>
-                Vowel quality is one of the biggest influences on what your
-                accent sounds like. Use this page to practice your vowel sounds
-                and get feedback on how close you are to the target.
-                <br />
-                To calibrate the tool, first record yourself saying each of the
-                four corner vowels: <strong>i</strong> (as in <em>see</em>),&nbsp;
-                <strong>u</strong> (as in <em>blue</em>),&nbsp;
-                <strong>a</strong> (as in <em>father</em>), and&nbsp;
-                <strong>æ</strong> (as in <em>cat</em>).
-            </p>
-            <VowelCalibrator />
-            <VowelChart />
-        </div>
+            <div className="page-text">
+                <h1>Practice Your Pronunciation</h1>
+                <p>
+                    Vowel quality is one of the biggest influences on what your
+                    accent sounds like. Use this page to practice your vowel sounds
+                    and get feedback on how close you are to the target.
+                    <br />
+                    To calibrate the tool, first record yourself saying each of the
+                    four corner vowels: <strong>i</strong> (as in <em>see</em>),&nbsp;
+                    <strong>u</strong> (as in <em>blue</em>),&nbsp;
+                    <strong>a</strong> (as in <em>father</em>), and&nbsp;
+                    <strong>æ</strong> (as in <em>cat</em>).
+                </p>
+                <VowelCalibrator />
+                <VowelChart />
+            </div>
+        </>
     );
 };
