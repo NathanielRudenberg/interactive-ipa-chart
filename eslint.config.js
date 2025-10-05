e1123143c6c8cd476f25a1c2d17f0bd297fe2658
Nathaniel Rudenberg <nathaniel.rudenberg@gordon.edu>
2025-10-05 14:50:28 -0400

    Add eslint config

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+// eslint.config.js
+import globals from 'globals';
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+
+export default defineConfig([
+    {
+        files: ["**/*.js"], // Apply this configuration to all .js files
+        plugins: {
+            js,
+        },
+        extends: ["js/recommended"], // Extend recommended ESLint rules
+        languageOptions: {
+            globals: {
+                ...globals.node
+            }
+        },
+        rules: {
+            "no-unused-vars": "warn", // Custom rule: warn about unused variables
+            "no-undef": "warn", // Custom rule: warn about undefined variables
+        },
+    },
+    // You can add more configuration objects for different file types or overrides
+]);
