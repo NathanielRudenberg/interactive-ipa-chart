1e4c137ad89bad02dbb7364686031e2f13a598ee
Nathaniel Rudenberg <nathaniel.rudenberg@gordon.edu>
2025-10-08 01:00:57 -0400

    Change material-ui theme

--- src/index.jsx
+++ src/index.jsx
@@ -7,12 +7,52 @@ import * as serviceWorker from './serviceWorker';
 
 const appTheme = createTheme(({
     palette: {
+        background: {
+            default: '#FDF6EC'
+        },
         primary: {
             main: '#CBD4C2',
         },
         secondary: {
             main: '#967AA1'
         }
+    },
+    typography: {
+        h1: {
+            fontSize: '2em',
+            fontWeight: 700,
+            marginBlockStart: '0.67em',
+            marginBlockEnd: '0.67em'
+        },
+        h2: {
+            fontSize: '1.5em',
+            fontWeight: 700,
+            marginBlockStart: '0.83em',
+            marginBlockEnd: '0.83em'
+        },
+        h4: {
+            fontSize: '1em',
+            fontWeight: 700,
+            marginBlockStart: '1.33em',
+            marginBlockEnd: '1.33em'
+        },
+        body1: {
+            lineHeight: 1.8
+        },
+        fontFamily: [
+            'DejaVu Sans',
+            '-apple-system',
+            'BlinkMacSystemFont',
+            'Segoe UI',
+            'Roboto',
+            'Oxygen',
+            'Ubuntu',
+            'Cantarell',
+            'Fira Sans',
+            'Droid Sans',
+            'Helvetica Neue',
+            'sans-serif'
+        ].join(',')
     }
 }))
 
