0a5a3fd5d1e5b5f65993d656321b8e9027f8d612
Nathaniel Rudenberg <nathaniel.rudenberg@gordon.edu>
2025-10-08 15:24:32 -0400

    Prompt the user to calibrate the app for their voice

--- src/components/CalibrationModal/index.jsx
+++ src/components/CalibrationModal/index.jsx
@@ -6,6 +6,14 @@ import Button from '@mui/material/Button';
 import Alert from '@mui/material/Alert';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import Divider from '@mui/material/Divider';
+import { ThreeCircles, TailSpin, Grid, Audio } from 'react-loader-spinner';
+import { ReactMediaRecorder } from 'react-media-recorder';
+import AudioRecorder from '../AudioRecorder/';
+import pythonCodeUrl from '../../services/user_vowelspace.py?url';
+import { fetchFile } from '@ffmpeg/util';
+import { useFFmpeg } from '../../hooks/useFFmpeg'
+import { usePyodide } from '../../hooks/usePyodide';
 
 const INTRO_STEP = 'intro';
 const RECORD_STEP = 'record';
@@ -13,8 +21,13 @@ const CALIBRATE_STEP = 'calibrate';
 const DONE_STEP = 'done';
 
 const CalibrationModal = () => {
-    const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(true);
     const [currentStep, setCurrentStep] = useState(INTRO_STEP);
+    const [recordings, setRecordings] = useState({});
+    const [isRunning, setIsRunning] = useState(false);
+    const [error, setError] = useState(null);
+    const { ffmpeg, isFFmpegReady } = useFFmpeg();
+    const { pyodide, isPyodideReady } = usePyodide();
 
     const goToRecord = () => {
         setCurrentStep(RECORD_STEP);
@@ -24,12 +37,85 @@ const CalibrationModal = () => {
         setCurrentStep(INTRO_STEP);
     };
 
+    const goToCalibration = () => setCurrentStep(CALIBRATE_STEP);
+
+    const done = () => setCurrentStep(DONE_STEP)
+
+    async function getCode() {
+        const code = await (await fetch(pythonCodeUrl)).text();
+        return code;
+    }
+
+    const handleCalibrate = async () => {
+        try {
+            // Store audio files in pyodide's virtual filesystem
+            for (const name in recordings) {
+                const fileData = await recordings[name].wavBlob.arrayBuffer();
+                pyodide.storeFile(`/audio/practiceCalibration/${name}.wav`, new Uint8Array(fileData));
+            }
+
+            // Load and run the calibration script
+            setIsRunning(true);
+            const pythonCode = await getCode();
+            const jsonStringResult = await pyodide.run(pythonCode);
+
+            // Check if we got a valid string before parsing
+            if (!jsonStringResult) {
+                throw new Error("Python script returned no result.");
+            }
+
+            const results = JSON.parse(jsonStringResult);
+            console.log('Calibration results:', results);
+
+            if (results && results.stats) {
+                pyodide.setGlobal("SPEAKER_STATS", results.stats);
+                console.log('Speaker stats saved to global variable "SPEAKER_STATS"');
+            }
+
+        } catch (Error) {
+            console.error('Calibration failed:', Error);
+        } finally {
+            setIsRunning(false);
+        }
+    }
+
+    const handleStop = async (blobUrl, blob) => {
+        try {
+            const recordingName = `recording_${Date.now()}`;
+            const inputFileName = `${recordingName}.webm`;
+            const outputFileName = `${recordingName}.wav`;
+
+            await ffmpeg.writeFile(inputFileName, await fetchFile(blob));
+            await ffmpeg.exec(['-i', inputFileName, '-ac', '1', '-ar', '16000', outputFileName]);
+
+            const data = await ffmpeg.readFile(outputFileName);
+
+            const wavBlob = new Blob([data.buffer], { type: 'audio/wav' });
+            const wavUrl = URL.createObjectURL(wavBlob);
+
+            setRecordings(prevRecordings => ({
+                ...prevRecordings,
+                [recordingName]: {
+                    url: wavUrl,
+                    blob: blob,
+                    wavBlob: wavBlob,
+                }
+            }));
+        } catch (error) {
+            console.error('Error processing recording:', error);
+        }
+    };
+
     const style = {
         position: 'absolute',
         top: '50%',
         left: '50%',
+        display: 'flex',
+        flexDirection: 'column',
         transform: 'translate(-50%, -50%)',
         width: 600,
+        height: 400,
+        overflowY: 'auto',
         bgcolor: 'background.default',
         borderRadius: '8px',
         // border: '2px solid #000',
@@ -39,13 +125,12 @@ const CalibrationModal = () => {
     };
 
     const introStep = (
-        <Box sx={style}>
-            <Typography variant="h2">Your Vocal Signature</Typography>
-            <Typography variant="body1">
+        <>
+            <Typography variant="body1" >
                 To get started, we need to learn the unique sound of your voice.
                 This quick process maps your accent.
                 On the next screen, record yourself saying the following four vowel sounds:
-            </Typography>
+            </Typography >
             <Typography variant='body1'>
                 <strong>i</strong> (as in <em>see</em>)
             </Typography>
@@ -61,7 +146,7 @@ const CalibrationModal = () => {
             <Alert severity='info'>
                 Tip: Record in a quiet room, and hold each sound for 1-2 seconds.
             </Alert>
-            <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <Box sx={{ display: 'flex', justifyContent: 'flex-end', marginTop: 'auto' }}>
                 <Button
                     onClick={goToRecord}
                     disableRipple
@@ -73,13 +158,17 @@ const CalibrationModal = () => {
                     Next
                 </Button>
             </Box>
-        </Box>
+        </>
     );
 
     const recordStep = (
-        <Box sx={style}>
-            <Typography variant='h2'>Palbo</Typography>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <>
+            <ReactMediaRecorder
+                audio
+                onStop={handleStop}
+                render={props => <AudioRecorder {...props} recordings={recordings} />}
+            />
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', marginTop: 'auto' }}>
                 <Button
                     color="secondary"
                     variant="contained"
@@ -93,17 +182,43 @@ const CalibrationModal = () => {
                 <Button
                     color="secondary"
                     variant="contained"
-                    onClick={() => { }}
+                    onClick={() => {
+                        handleCalibrate()
+                        goToCalibration()
+                    }}
                     sx={{ marginTop: '8px' }}
                     disableRipple
                     endIcon={<ArrowForwardIcon />}
+                    disabled={Object.keys(recordings).length < 4}
                 >
                     Next
                 </Button>
             </Box>
-        </Box>
+        </>
     );
 
+    const calibrateStep = isRunning ? (
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <Grid color="#CBD4C2" />
+        </div>
+    ) : (
+        <>
+            <Typography>Ready!</Typography>
+            <Box sx={{ display: 'flex', justifyContent: 'flex-end', marginTop: 'auto' }}>
+                <Button
+                    color="primary"
+                    variant="contained"
+                    onClick={done}
+                    sx={{ marginTop: '8px' }}
+                    disableRipple
+                    // endIcon={<ArrowForwardIcon />}
+                    disabled={Object.keys(recordings).length < 4 || !isPyodideReady || !isFFmpegReady || isRunning}
+                >
+                    Done
+                </Button>
+            </Box>
+        </>
+    )
 
     let modalContent;
     switch (currentStep) {
@@ -113,6 +228,11 @@ const CalibrationModal = () => {
         case RECORD_STEP:
             modalContent = recordStep;
             break;
+        case CALIBRATE_STEP:
+            modalContent = calibrateStep;
+            break;
+        default:
+            modalContent = introStep;
     }
 
     const handleOpen = () => setIsOpen(true)
@@ -121,10 +241,14 @@ const CalibrationModal = () => {
         <>
             <Button variant={"contained"} onClick={handleOpen}>Pablo</Button>
             <Modal
-                open={isOpen}
+                open={currentStep !== DONE_STEP}
                 onClose={handleClose}
             >
-                {modalContent}
+                <Box sx={style}>
+                    <Typography variant="h2">Your Vocal Signature</Typography>
+                    <Divider sx={{ margin: '16px 0' }} />
+                    {modalContent}
+                </Box>
             </Modal>
         </>
     );
--- src/views/Practice/index.jsx
+++ src/views/Practice/index.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import Python from '../../components/Python';
 import VowelChart from '../../components/IPAChart/VowelChart';
-import VowelCalibrator from '../../components/VowelCalibrator';
 import UserVowelTester from '../../components/UserVowelTester';
 import Button from '@mui/material/Button';
 import CalibrationModal from '../../components/CalibrationModal';
@@ -13,22 +12,9 @@ export default function Practice() {
         <>
             <CalibrationModal />
             <div id="plot-container" />
-            <Python />
+            {/* <Python /> */}
             <div className="page-text">
                 <h1>Practice Your Pronunciation</h1>
-                <p>
-                    Vowel quality is one of the biggest influences on what your
-                    accent sounds like. Use this page to practice your vowel sounds
-                    and get feedback on how close you are to the target.
-                    <br />
-                    To calibrate the tool, first record yourself saying each of the
-                    four corner vowels: <strong>i</strong> (as in <em>see</em>),&nbsp;
-                    <strong>u</strong> (as in <em>blue</em>),&nbsp;
-                    <strong>a</strong> (as in <em>father</em>), and&nbsp;
-                    <strong>æ</strong> (as in <em>cat</em>).
-                </p>
-                <VowelCalibrator />
-                <h3>Plotting Your Own Voice</h3>
                 <UserVowelTester setVowelValue={setUserVowels} />
                 <VowelChart extras={userVowels} />
             </div>
