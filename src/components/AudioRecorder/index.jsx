a001c7dcb66cedec5eb97baa60ea5a72ffd14548
Nathaniel Rudenberg <nathaniel.rudenberg@gordon.edu>
2025-10-09 12:12:59 -0400

    Don't show saved recordings

--- src/components/AudioRecorder/index.jsx
+++ src/components/AudioRecorder/index.jsx
@@ -2,7 +2,7 @@ import Button from '@mui/material/Button';
 import MicIcon from '@mui/icons-material/Mic';
 import StopIcon from '@mui/icons-material/Stop';
 
-const AudioRecorder = ({ status, startRecording, stopRecording, recordings, mediaBlobUrl }) => {
+const AudioRecorder = ({ status, startRecording, stopRecording, recordings, setStatus, mediaBlobUrl }) => {
     const recordingButton = status === "recording" ? (
         <Button
             onClick={stopRecording}
@@ -27,13 +27,13 @@ const AudioRecorder = ({ status, startRecording, stopRecording, recordings, medi
             <p>{status}</p>
             {recordingButton}
 
-            <h2>Saved Recordings:</h2>
-            {Object.entries(recordings).map(([name, recording]) => (
-                <div key={name}>
-                    <p>{name}</p>
-                    <audio src={recording.url} controls />
-                </div>
-            ))}
+            {/* <h2>Saved Recordings:</h2> */}
+            {/* {Object.entries(recordings).map(([name, recording]) => ( */}
+            {/*     <div key={name}> */}
+            {/*         <p>{name}</p> */}
+            {/*         <audio src={recording.url} controls /> */}
+            {/*     </div> */}
+            {/* ))} */}
         </div>
     )
 }
