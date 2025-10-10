67ef62aa067aabd1a1c2abe9bc491a53d7a28a35
Nathaniel Rudenberg <nathaniel.rudenberg@gordon.edu>
2025-10-10 19:31:53 -0400

    Consolidate recording button definitions

--- src/components/AudioRecorder/index.jsx
+++ src/components/AudioRecorder/index.jsx
@@ -3,37 +3,38 @@ import MicIcon from '@mui/icons-material/Mic';
 import StopIcon from '@mui/icons-material/Stop';
 
 const AudioRecorder = ({ status, startRecording, stopRecording, recordings, setStatus, mediaBlobUrl }) => {
-    const recordingButton = status === "recording" ? (
+    let Icon;
+    if (status === "recording") {
+        Icon = StopIcon;
+    } else if (status === "acquiring_media") {
+        Icon = StopIcon;
+    } else {
+        Icon = MicIcon;
+    }
+
+    const onClick = status === "recording" ? stopRecording : startRecording;
+
+    const recordingButton = (
         <Button
-            onClick={stopRecording}
+            onClick={onClick}
             variant="contained"
             color="secondary"
             disableRipple
         >
-            <StopIcon />
-        </Button>
-    ) : (
-        <Button
-            onClick={startRecording}
-            variant="contained"
-            color="secondary"
-            disableRipple
-        >
-            <MicIcon />
+            <Icon />
         </Button>
     );
     return (
         <div>
-            <p>{status}</p>
-            {recordingButton}
-
-            {/* <h2>Saved Recordings:</h2> */}
-            {/* {Object.entries(recordings).map(([name, recording]) => ( */}
-            {/*     <div key={name}> */}
-            {/*         <p>{name}</p> */}
-            {/*         <audio src={recording.url} controls /> */}
-            {/*     </div> */}
-            {/* ))} */}
+            {/* <p>{status}</p> */}
+            <Button
+                onClick={onClick}
+                variant="contained"
+                color="secondary"
+                disableRipple
+            >
+                <Icon />
+            </Button>
         </div>
     )
 }
