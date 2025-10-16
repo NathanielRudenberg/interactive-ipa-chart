4362d68eeec4314ba120ef1c2e56d6189cfcc434
Nathaniel Rudenberg <nathaniel.rudenberg@gordon.edu>
2025-10-16 19:45:01 -0400

    Adjust whitespace, I think

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,20 +1,17 @@
-import React, { useState } from 'react';
-import LanguageSelector from '../../components/LanguageSelector';
 import IPAChart from '../../components/IPAChart';
-import { useLanguage } from '../../components/LanguageProvider';
 
- export default function Home() {
+export default function Home() {
 
-//   componentDidMount() {
-//     let currentPath = window.location.pathname;
-//     if ((/^\/languages\/.+/).test(currentPath)) this.setState({languageSelected: true});
-//   }
+    //   componentDidMount() {
+    //     let currentPath = window.location.pathname;
+    //     if ((/^\/languages\/.+/).test(currentPath)) this.setState({languageSelected: true});
+    //   }
 
     return (
-            <div className='languages-container'>
-                <div className={'chart-container'} >
-                    <IPAChart default={false} />
-                </div>
+        <div className='languages-container'>
+            <div className={'chart-container'} >
+                <IPAChart default={false} />
             </div>
+        </div>
     );
 };
